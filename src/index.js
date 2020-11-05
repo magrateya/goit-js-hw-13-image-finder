dddeb5b3e7fb34a9df7de0e00f9e3443d47242eb
Maria Trishch <magrateya@meta.ua>
2020-11-05 23:47:03 +0200

    add notify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './css/styles.css';
 import itemTpl from './templates/items.hbs';
 import DataApiService from './js/apiService';
-import LoadMoreBtn from './js/load-more'
+import LoadMoreBtn from './js/load-more';
+
+import '@pnotify/core/dist/PNotify.css';
+import { error, info } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
 
 
 const refs = {
@@ -32,16 +36,19 @@ function onSearch(e) {
     clearGalleryContainer();
 
     fetchArticles();
+    info({ text: "Запит успішний.", delay: 1000 });
 }
 
 function fetchArticles() {
     loadMoreBtn.disable();
     dataApiService.fetchData().then(hits => {
+        if (hits.length === 0) {
+            error({ text: "За вашим запитом нічого не знайдено, спробуйте ще.", delay: 2000 }); 
+        }
         appendGalleryMarkup(hits);
         loadMoreBtn.enable();
         scrollDown();
     });
-  
 }
 
 function appendGalleryMarkup(hits) {
