4e648447e5938bcd9704b657d245a3f83e867633
Maria Trishch <magrateya@meta.ua>
2020-11-06 15:11:30 +0200

    fix load more btn

--- src/index.js
+++ src/index.js
@@ -6,49 +6,57 @@ import LoadMoreBtn from './js/load-more';
 import '@pnotify/core/dist/PNotify.css';
 import { error, info } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
-
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    galleryContainer: document.querySelector('.gallery'),
-}
+  searchForm: document.querySelector('.js-search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+};
 const loadMoreBtn = new LoadMoreBtn({
-    selector: '[data-action="load-more"]',
-    hidden: true,
+  selector: '[data-action="load-more"]',
+  hidden: true,
 });
 
 const dataApiService = new DataApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+refs.galleryContainer.addEventListener('click', onModalOpen);
 
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    dataApiService.query = e.currentTarget.elements.query.value;
+  dataApiService.query = e.currentTarget.elements.query.value;
 
-    if (dataApiService.query === '') {
-        return;
-    }
+  if (dataApiService.query === '') {
+    return;
+  }
 
-    loadMoreBtn.show();
-    dataApiService.restPage();
-    clearGalleryContainer();
+  loadMoreBtn.show();
+  dataApiService.restPage();
+  clearGalleryContainer();
 
-    fetchArticles();
-    info({ text: "Запит успішний.", delay: 1000 });
+  fetchArticles();
+  info({ text: 'Запит успішний.', delay: 1000 });
 }
 
 function fetchArticles() {
-    loadMoreBtn.disable();
-    dataApiService.fetchData().then(hits => {
-        if (hits.length === 0) {
-            error({ text: "За вашим запитом нічого не знайдено, спробуйте ще.", delay: 2000 }); 
-        }
-        appendGalleryMarkup(hits);
-        loadMoreBtn.enable();
-        scrollDown();
-    });
+  loadMoreBtn.disable();
+  dataApiService.fetchData().then(hits => {
+    if (hits.length === 0) {
+      error({
+        text: 'За вашим запитом нічого не знайдено, спробуйте ще.',
+        delay: 2000,
+      });
+    } else if (hits.length < 12) {
+      info({ text: 'not much...', delay: 1000 });
+      loadMoreBtn.hide();
+    }
+    appendGalleryMarkup(hits);
+    loadMoreBtn.enable();
+    scrollDown();
+  });
 }
 
 function appendGalleryMarkup(hits) {
@@ -60,22 +68,17 @@ function clearGalleryContainer() {
 }
 
 function scrollDown() {
-    window.scrollBy({
-        top: document.documentElement.clientHeight,
-        behavior: 'smooth'
-        });
+  window.scrollBy({
+    top: document.documentElement.clientHeight,
+    behavior: 'smooth',
+  });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function onModalOpen(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const largeImageURL = `<img src= ${e.target.dataset.url}>`;
+  console.log(largeImageURL);
+  basicLightbox.create(largeImageURL).show();
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,32 +1,32 @@
-
 export default class DataApiService {
-    constructor() {
-        this.searchInput = '';
-        this.page = 1;
-    }
+  constructor() {
+    this.searchInput = '';
+    this.page = 1;
+  }
 
-    fetchData() {
-        
-        const BASE_URL = 'https://pixabay.com/api/';
-        const API_KEY = '18994558-99c21eb2af8503bc6443a1f41';
+  fetchData() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '18994558-99c21eb2af8503bc6443a1f41';
 
     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchInput}&page=${this.page}&per_page=12&key=${API_KEY}`;
 
-        return fetch(url).then(responce => responce.json()).then(({hits}) => {
-            this.page += 1;
-            return hits;
-    });
-    }
+    return fetch(url)
+      .then(responce => responce.json())
+      .then(({ hits }) => {
+        this.page += 1;
+        return hits;
+      });
+  }
 
-    restPage() {
-        this.page = 1;
-    }
+  restPage() {
+    this.page = 1;
+  }
 
-    get query() {
-       return this.searchInput;
-    }
+  get query() {
+    return this.searchInput;
+  }
 
-    set query(newQuery) {
-        this.searchInput = newQuery;
-    }
+  set query(newQuery) {
+    this.searchInput = newQuery;
+  }
 }
